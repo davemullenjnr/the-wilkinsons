26f423c8778c0182ede0050f0aeeec454a7143a3
davemullenjnr <davemullenjnr@gmail.com>
2019-08-28 00:07:44 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,16 +18,24 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `mr-and-mrs-wilkinson`,
+        short_name: `the-wilkinsons`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#665E5C`,
+        theme_color: `#E7D0CD`,
         display: `minimal-ui`,
         icon: `src/assets/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-56108422-2",
+      },
+    },
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify`,
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
