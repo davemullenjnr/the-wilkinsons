1adf5c58a1cfe7b916403c26a18a27fd4cebe363
davemullenjnr <davemullenjnr@gmail.com>
2019-08-28 19:29:46 +0100

    Update images.js

--- src/components/images.js
+++ src/components/images.js
@@ -5,11 +5,12 @@ import styled from 'styled-components'
 import { variables } from '../theme';
 
 const Container = styled.main`
-  padding: 0 ${variables.spacer * 4}rem;
+  padding: 0 ${variables.spacer * 2}rem;
   margin: 0 auto;
   margin-bottom: 0;
 
   @media ${variables.md} {
+    padding: 0 ${variables.spacer * 4}rem;
     max-width: ${variables.spacer * 96}rem;
     margin-bottom: ${variables.spacer * 16}rem;
   }
@@ -21,10 +22,14 @@ const Container = styled.main`
 
 const Image = styled(Img)`
 border-radius: ${variables.spacer * 0.25}rem;
-margin-bottom: ${variables.spacer * 4}rem;
+margin-bottom: ${variables.spacer * 2}rem;
 
   &:last-child {
     margin-bottom: 0;
+  }
+
+  @media ${variables.md} {
+    margin-bottom: ${variables.spacer * 4}rem;
   }
 `
 
@@ -36,8 +41,8 @@ const Images = () => (
           edges {
             node {
               childImageSharp {
-                fluid(maxWidth: 2160, quality: 90) {
-                  ...GatsbyImageSharpFluid_noBase64
+                fluid(maxWidth: 2160, quality: 70) {
+                  ...GatsbyImageSharpFluid
                 }
               }
             }
@@ -48,7 +53,7 @@ const Images = () => (
     render={data => (
         <Container>
           {data.source.edges.map(({ node }, i) => (
-              <Image key={i} fluid={node.childImageSharp.fluid} backgroundColor={variables.light} />
+              <Image key={i} fluid={node.childImageSharp.fluid} loading="auto" backgroundColor="rgba(240, 233, 233, 1)" />
           ))}
         </Container>
     )}
